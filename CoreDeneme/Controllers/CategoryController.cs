206ea0204a28796ba3e0c7da990c08ae0040cfaa
KaanALKANAT <100507109+KaanALKANAT@users.noreply.github.com>
2022-03-20 21:22:46 +0300

    InterfaceEklendi

---
+++ CoreDeneme/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDeneme.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepository cr = new CategoryRepository();
+        public IActionResult Index()
+        {
+            return View(cr.ListCategory());
+        }
+    }
+}
